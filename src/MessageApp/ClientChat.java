ec3340632ee74e55fce6689c59694e3583da7f4c
Janith Niroshan <129001487+neroIJN@users.noreply.github.com>
2024-12-20 10:28:48 +0530

    done full logics

--- src/MessageApp/ClientChat.java
+++ src/MessageApp/ClientChat.java
@@ -3,8 +3,10 @@ package MessageApp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientChat {
     static int PORT = 9999;
@@ -12,24 +14,32 @@ public class ClientChat {
         System.out.println("\t\t Simple Client");
         System.out.println("\t\t ====================");
 
+        var scannerInput = new Scanner(System.in);
+        String input,output;
+
+
         // Get the local host's IP address
         var ipAddress = InetAddress.getLocalHost();
         // Create a socket and connect to the server at the specified port
         var socket = new Socket(ipAddress,PORT);
-        System.out.println("\t\t server is connected");
 
-        // Create a BufferedReader to read input from the server's input stream
-        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        // store in the input
-        String input = in.readLine();
-        //print
-        System.out.println("Server says:" + input);
+        var out = new PrintWriter(socket.getOutputStream(),true);
+        var in =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        System.out.println("Closing the connection");
-        socket.close();
-        in.close();
+        try{
+            while (true){
+                input = in.readLine();
+                System.out.println("Server Says "+ input );
 
-        System.exit(0);
-
+                System.out.println("Server :");
+                output = scannerInput.nextLine();
+                out.println(output);
+            }
+        }
+        finally {
+            out.close();
+            in.close();
+            socket.close();
+        }
     }
 }
