4b24e7cb17ea550a5b8f079c85f84c636e28f181
Janith Niroshan <129001487+neroIJN@users.noreply.github.com>
2024-12-20 10:06:01 +0530

    done ClientChat logic

--- src/MessageApp/ClientChat.java
+++ src/MessageApp/ClientChat.java
@@ -1,8 +1,35 @@
 package MessageApp;
 
-public class ClientChat {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class ClientChat {
+    static int PORT = 9999;
+    public static void main(String[] args) throws IOException {
+        System.out.println("\t\t Simple Client");
+        System.out.println("\t\t ====================");
+
+        // Get the local host's IP address
+        var ipAddress = InetAddress.getLocalHost();
+        // Create a socket and connect to the server at the specified port
+        var socket = new Socket(ipAddress,PORT);
+        System.out.println("\t\t server is connected");
+
+        // Create a BufferedReader to read input from the server's input stream
+        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // store in the input
+        String input = in.readLine();
+        //print
+        System.out.println("Server says:" + input);
+
+        System.out.println("Closing the connection");
+        socket.close();
+        in.close();
+
+        System.exit(0);
 
     }
 }
--- src/MessageApp/ServerChat.java
+++ src/MessageApp/ServerChat.java
@@ -1,6 +1,8 @@
 package MessageApp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.util.Scanner;
@@ -21,12 +23,24 @@ public class ServerChat {
 
                 var socket = serverSocket.accept();
                 System.out.println("\t\t Clinet is connected...\n\n");
+                var out = new PrintWriter(socket.getOutputStream(),true);
+                var in =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
                 try {
-                    var out = new PrintWriter(socket.getOutputStream(),true);
-                    out.println("Welcome to socket programming");
+                    out.println("Welcome to Chat Application");
+                    while (true){
+                        input = in.readLine();
+                        System.out.println("Client says "+ input);
+
+                        System.out.println("Client: ");
+                        output = scannerInput.nextLine();
+                        out.println(output);
+                    }
                 }
                 finally {
                     socket.close();
+                    out.close();
+                    in.close();
                 }
             }
         }
